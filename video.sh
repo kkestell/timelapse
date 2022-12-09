ec40067f2f434f9a81c43a65a1dc3b8fb5aa4ba0
Kyle Kestell <kyle@kestell.org>
2022-12-09 09:26:28 -0600

    Update video.sh

--- video.sh
+++ video.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-camera_name='Camera'
+camera_name='Deck'
 time=`date '+%Y-%m-%d-%H-%M-%S'`;
-root_directory="/home/kyle/timelapse/storage/$camera_name"
+root_directory="/home/kyle/Pictures/Timelapse/$camera_name"
 frames_directory="$root_directory/frames"
 videos_directory="$root_directory/videos"
 mkdir -p $videos_directory
 file_path="$videos_directory/$time.mp4"
 ffmpeg                                              \
     -framerate 30                                   \
-    -pattern_type glob -i "$frames_directory/*.jpg" \
+    -pattern_type glob -i "$frames_directory/**/*.jpg" \
     -c:v libx264                                    \
     -pix_fmt yuv420p                                \
     $file_path
